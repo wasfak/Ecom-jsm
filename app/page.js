875c5b1f0f664ca4850bc549268c564f7144dc51
wasfak <hardmodz@yahoo.com>
2024-03-19 11:51:06 +0200

    fonts

--- app/page.js
+++ app/page.js
@@ -1,10 +1,34 @@
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import PopularProducts from "@/components/PopularProducts";
+import Services from "@/components/Services";
+import SpecialOffer from "@/components/SpecialOffer";
+import Subscribe from "@/components/Subscribe";
+import SuperQuality from "@/components/SuperQuality";
 
 export default function Home() {
   return (
     <main className="relative">
       <section className="xl:padding-l wide:padding-r padding-b">
         <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className=" bg-black padding-x padding-t pb-8">
+        <Footer />
       </section>
     </main>
   );
